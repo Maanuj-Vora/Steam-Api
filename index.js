393f68d24e80c0619f68ea20c8feb703bb347e91
Maanuj Vora <maanujvora@gmail.com>
2022-02-06 23:34:40 -0800

    Basic Changes Api Updated

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require("express")
 const app = express()
-const product = require("./api/product")
+const development_changes = require("./development/changes")
+const development_info = require("./development/info")
 
+
+const helper = require("./helper")
 app.use(express.json({ extended: false }))
 
-app.use("/api/product", product)
+app.use(helper.cors);
+
+app.use("/development/changes", development_changes)
+app.use("/development/info", development_info)
+
+app.get("/", async (req, res) => {
+    try {
+        res.json({
+            status: 200,
+            message: "api is currently up and running",
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+});
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
