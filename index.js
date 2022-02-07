ff80989abe445f8f421f2a8d3512005df18bcf83
Maanuj Vora <maanujvora@gmail.com>
2022-02-07 02:28:26 -0800

    Added endpoint -> /query/information/:steam_appid for testing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require("express")
 const app = express()
 const development_changes = require("./development/changes")
 const development_info = require("./development/info")
+const query_information = require("./query/information")
 const helper = require("./helper")
 
 app.use(express.json({ extended: false }))
@@ -10,6 +14,7 @@ app.use(helper.cors);
 
 app.use("/development/changes", development_changes)
 app.use("/development/info", development_info)
+app.use("/query/information", query_information)
 
 app.get("/", async (req, res) => {
     try {
